5e76a228512fecf1be2448a05631023543ef144e
Christian De Freitas <harddiskonline1@gmail.com>
2022-08-20 22:22:01 -0400

    mods varios

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const length = document.querySelector("#time");
 const desc = document.querySelector("#desc");
 const veil = document.querySelector(".veil");
 const btnMore = document.querySelector("#more-less");
-
+var url = ''
 
 var ytData;
 async function getData() {
@@ -31,16 +31,103 @@ function printData(data) {
     ytData = JSON.parse(data);
     console.log(ytData);
 
+    //AGREGO INFO DEL CANAL
+    const infoC = document.querySelector('#chan-info')
+    infoC.innerHTML=''
+    const info = document.createElement('div')
+    const infoLink = document.createElement('a')
+    info.classList.add('info-channel')
+    info.innerText = ytData.channel
+    infoLink.href=ytData.channel_url
+    infoLink.append(info)
+    infoC.append(infoLink)
+
+    //AGREGO CAPÍTULOS SI LO TUVIESE
+    if (ytData.hasOwnProperty('chapters')){
+        printChapters(ytData.chapters)
+    }
+
+    //AGREGO LINK PARA VER EN YOUTUBE
+    const ytLink = document.querySelector('#link-yt')
+    ytLink.href = ytData.webpage_url
+    url = ytData.webpage_url
+
+
+    // const vFrame = document.querySelector('#video-container')
+    // const iframe = `
+    // <iframe  src="https://www.youtube.com/embed/${ytData.id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    // `
+
+    // vFrame.innerHTML += iframe;
+
     dataContainer.innerHTML = ytData.title + " (" + prettyTime(ytData.duration) + ")";
     img.src = ytData.thumbnail;
     // length.innerHTML = prettyTime(ytData.duration)
     desc.innerText = ytData.description
 
+    printTags(ytData.tags);
     drawTable(ytData.formats);
+}
+const printTags = (tags)=>{
+    const tagsC = document.querySelector('#tags')
+    tagsC.innerHTML=''
+    tags.forEach((tag)=>{
+        var tagContainer = document.createElement('div')
+        tagContainer.classList.add('tag')
+        tagContainer.innerText = tag
+        tagsC.append(tagContainer)
+    })
+}
+const printChapters = (chapters)=>{
+    const chaptersC = document.querySelector('#chapters')
+    const col61 = document.createElement('div')
+    col61.classList.add('col-6')
+    const col62 = document.createElement('div')
+    col62.classList.add('col-6')
+    const l = chapters.length
+    var c=0
+    chaptersC.innerHTML=''
+    chapters.forEach(chapter => {
+        var col=1
+        if (++c > Math.round(l/2)) col=2
+        const a = document.createElement('a')
+        a.classList.add('chapter')
+        a.style.display='block'
+        a.innerText = chapter.title
+        a.href = `${url}&t=&${chapter.start_time}`
+        a.target="_blank"
+
+        if (col == 1){
+            col61.appendChild(a)
+            chaptersC.appendChild(col61)
+        } else if (col == 2){
+            col62.appendChild(a)
+            chaptersC.appendChild(col62)
+        }
+    });
+    //setHeight to chapters
+    chapterHeight()
+}
+
+const chapterHeight = ()=>{
+    setTimeout(()=>{
+        const imgHeightRaw = document.querySelector("#img-thumb").clientHeight;
+        const imgHeight = parseFloat(imgHeightRaw);
+        const tagsHeightRaw = document.querySelector("#tags").clientHeight;
+        const tagsHeight = parseFloat(tagsHeightRaw);
+        const footHeightRaw = document.querySelector("#footer-info").clientHeight;
+        const footHeight = parseFloat(footHeightRaw);
+        const chapters = document.querySelector('#chapters')
+
+        const height = imgHeight - tagsHeight - footHeight + 24;
+        chapters.style.height = height + "px";
+        chapters.style.overflowY = 'scroll'
+    },50)
 }
 
 function drawTable(data) {
     const tbody = document.querySelector("#tbody");
+    tbody.innerHTML = ''
     data.forEach((format) => {
         const info = `
             <tr>
@@ -86,7 +173,7 @@ function msgAudioVideo(format) {
 
 function moreDesc() {
     if (desc.classList.contains("txt-small")) {
-        desc.style.maxHeight = "2000px";
+        desc.style.maxHeight = "2000vh";
         desc.classList.remove("txt-small");
         desc.classList.add("txt-all");
         btnMore.innerText = "Less";
@@ -116,3 +203,8 @@ function prettyTime(sec) {
 
     return h + ":" + m + ":" + s;
 }
+
+onresize = () => {
+    console.log('resized')
+    chapterHeight()
+};
